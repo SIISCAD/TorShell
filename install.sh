50a3a036ec0f80485ead3bff056e44296cab53cb
Rodolfo Ramírez Acosta <zarattustra@gmail.com>
2018-05-09 12:43:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 wget -O /tmp/torShell.zip https://github.com/SIISCAD/TorShell/archive/master.zip
 unzip /tmp/torShell.zip -d /tmp/torShell_masterdir
 rm /tmp/torShell.zip
+
+### DELETE PREVIOUS INSTALATIONS ###
+rm -r $HOME/.local/share/gnome-shell/extensions/TorShell@siiscad.com.mx
+
+### INSTALL NEW ONE ###
+
 mv /tmp/torShell_masterdir/TorShell-master/TorShell@siiscad.com.mx $HOME/.local/share/gnome-shell/extensions
 rm -rf /tmp/torShell_masterdir/
 
